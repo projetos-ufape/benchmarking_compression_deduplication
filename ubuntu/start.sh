be7c3ffe16e525f912593ea72d5dcdb17fceedcd
Luan Zacarias <luanzacarias215@gmail.com>
2025-04-10 17:58:19 -0300

    feat: ajuste para obter dados de entrada e saida e valores para taxa de compresssao

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -8,17 +8,30 @@ do
     echo "round - $round"
     tech=zip
     key=guide_test-zip
+    mkdir -p resultados-$tech
+
     /usr/src/app/monitoramento.sh $round $tech $key &
-    echo `zip -v /usr/src/app/data/GUIDE_Test.zip /usr/src/app/data/GUIDE_Test.csv` > resultados-$key-$round.txt
+    
+    arquivo_original="/usr/src/app/data/GUIDE_Test.csv"
+    arquivo_comprimido="/usr/src/app/data/GUIDE_Test.zip"
+    strace -c -e trace=read,write,open zip -v $arquivo_comprimido $arquivo_original > /dev/null 2> resultados-$tech/resultado-$key-$round.txt
 
     echo "finding monitoramento.sh..."
     zip_pid=$(pgrep monitoramento.s)
     kill -TERM $zip_pid
     echo "killed monitoramento.sh..."
-    rm /usr/src/app/data/GUIDE_Test.zip
+
+
+    tamanho_original=$(stat -c %s "$arquivo_original")
+    tamanho_comprimido=$(stat -c %s "$arquivo_comprimido")
+
+    taxa=$(echo "scale=4; 1 - ($tamanho_comprimido / $tamanho_original)" | bc)
+    taxa_percentual=$(echo "scale=2; $taxa * 100" | bc)
+    echo "" >> resultados-$tech/resultado-$key-$round.txt
+    echo "original reduzido taxa" >> resultados-$tech/resultado-$key-$round.txt
+    echo "$tamanho_original $tamanho_comprimido $taxa_percentual" >> resultados-$tech/resultado-$key-$round.txt
+
+    rm arquivo_comprimido
     round=`expr $round + 1`
 done
 
-
-# monitoramento_zip_pid=$(pgrep monitoramento.zip.sh)  
-# kill -TERM $monitoramento_zip_pid
