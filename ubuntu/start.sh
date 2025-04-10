377a41ce91a0806c999ff74742612a888b378aee
Luan Zacarias <luanzacarias215@gmail.com>
2025-04-10 00:44:43 -0300

    feat: fluxo base para obter os dados feito

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -3,13 +3,19 @@ echo "Starting..."
 
 round=1
 
-while [ $round -lt 2 ]
+while [ $round -lt 3 ]
 do
     echo "round - $round"
-    /usr/src/app/monitoramento.sh $round zip zip &
+    tech=zip
+    key=guide_test-zip
+    /usr/src/app/monitoramento.sh $round $tech $key &
+    echo `zip -v /usr/src/app/data/GUIDE_Test.zip /usr/src/app/data/GUIDE_Test.csv` > resultados-$key-$round.txt
 
-    echo `zip -v /usr/src/app/data/GUIDE_Test.zip /usr/src/app/data/GUIDE_Test.csv` > loggg.txt
-
+    echo "finding monitoramento.sh..."
+    zip_pid=$(pgrep monitoramento.s)
+    kill -TERM $zip_pid
+    echo "killed monitoramento.sh..."
+    rm /usr/src/app/data/GUIDE_Test.zip
     round=`expr $round + 1`
 done
 
