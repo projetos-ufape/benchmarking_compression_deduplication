56e670972f92525c2ba788a44c076385871eb80a
Luan Zacarias <luanzacarias215@gmail.com>
2025-04-12 11:55:50 -0300

    feat: script de execussao em massa

--- Makefile
+++ Makefile
@@ -3,6 +3,13 @@ up:
 		echo "Erro: Variáveis TECH, FILENAME ou ROUND não definidas."; \
 		exit 1; \
 	fi
+	docker-compose up
+
+up-build:
+	@if [ -z "$$TECH" ] || [ -z "$$FILENAME" ] || [ -z "$$ROUND" ]; then \
+		echo "Erro: Variáveis TECH, FILENAME ou ROUND não definidas."; \
+		exit 1; \
+	fi
 	docker-compose up --build
 
 down:
--- start-docker.sh
+++ start-docker.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 echo "Starting service..."
 
-export TECH=borg_7z
-export FILENAME=GUIDE_Test.csv
-export ROUND=1
+# Lista de tecnologias
+# isoladas | compactação_deduplicação | deduplicação_compactação
+techs=("zip" "7z" "gzip" "bzip2" "borg" "restic" "zbackup" \
+    "zip_borg" "zip_restic" "zip_zbackup" \
+    "7z_borg" "7z_restic" "7z_zbackup" \
+    "gzip_borg" "gzip_restic" "gzip_zbackup" \
+    "bzip2_borg" "bzip2_restic" "bzip2_zbackup" \
+    "borg_zip" "borg_7z" "borg_gzip" "borg_bzip2" \
+    "restic_zip" "restic_7z" "restic_gzip" "restic_bzip2" \
+    "zbackup_zip" "zbackup_7z" "zbackup_gzip" "zbackup_bzip2" \
+    )
 
+# Lista de arquivos
+files=("GUIDE_Test.csv" "linux-master-clone")
 
-while [ $ROUND -le 2 ]
-do
-    make up
-    ROUND=`expr $ROUND + 1`
+make down
+make up-build
+
+for tech in "${techs[@]}"; do
+    export TECH="$tech"
+
+    for file in "${files[@]}"; do
+        export FILENAME="$file"
+
+        for round in {1..10}; do
+            export ROUND="$round"
+
+            echo "Executando: TECH=$TECH, FILENAME=$FILENAME, ROUND=$ROUND"
+
+            make up-build
+            make down
+        done
+    done
 done
