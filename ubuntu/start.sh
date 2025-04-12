5775a3f6fa321685ec3a83eaadd5ac1103399fb4
Luanzacarias <luanzacarias215@gmail.com>
2025-04-12 16:26:31 -0300

    fix: ajustes para excluir arquivos temporarios

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -13,7 +13,7 @@ base_name=""
 result_path="resultados/resultados-$techs"
 result_file=""
 
-IFS='_' read -r -a TECH_ARRAY <<< "$1"
+IFS='_' read -r -a TECH_ARRAY <<< "$techs"
 
 for tech in "${TECH_ARRAY[@]}"; do
     echo "rodando - $tech"
@@ -24,6 +24,13 @@ for tech in "${TECH_ARRAY[@]}"; do
     file_compressed=""
     result_file="resultado-$base_name-$tech-$round.txt"
 
+    is_dir=false
+    if [ -d "/usr/src/app/data/$temp_file" ]; then
+        is_dir=true
+    fi
+
+    echo "$origin_file $temp_file , $base_name "
+
     case "$tech" in
         zip)
             suffix="zip"
@@ -32,11 +39,21 @@ for tech in "${TECH_ARRAY[@]}"; do
             ;;
         gzip)
             suffix="gz"
+            if $is_dir; then
+                tar_file="$base_name.tar"
+                tar -cf "/usr/src/app/data/$tar_file" -C "/usr/src/app/data" "$temp_file" 
+                temp_file="$tar_file"
+            fi
             file_compressed="$base_name.$suffix"
             cmd="gzip -c /usr/src/app/data/$temp_file > /usr/src/app/data/$file_compressed"
             ;;
         bzip2)
             suffix="bz2"
+            if $is_dir; then
+                tar_file="$base_name.tar"
+                tar -cf "/usr/src/app/data/$tar_file" -C "/usr/src/app/data" "$temp_file" 
+                temp_file="$tar_file"
+            fi
             file_compressed="$base_name.$suffix"
             cmd="bzip2 -c /usr/src/app/data/$temp_file > /usr/src/app/data/$file_compressed"
             ;;
@@ -47,30 +64,30 @@ for tech in "${TECH_ARRAY[@]}"; do
             ;;
         borg)
             suffix="borg"
-            repo="tmp/$base_name"
+            repo="tmp/${base_name}-${round}"
             mkdir -p "/usr/src/app/data/$repo"
             export BORG_PASSPHRASE="test"
             borg init --encryption=none "/usr/src/app/data/$repo"
             cmd="borg create /usr/src/app/data/$repo::backup-round-$round /usr/src/app/data/$temp_file"
-            file_compressed=$repo
+            file_compressed="$repo"
+            du -bcs "/usr/src/app/data/$repo" > "/usr/src/app/data/${repo}.size"
             ;;
         restic)
             suffix="restic"
-            repo="tmp/$base_name"
+            repo="tmp/${base_name}-${round}"
             mkdir -p "/usr/src/app/data/$repo"
             export RESTIC_PASSWORD="test"
             export RESTIC_REPOSITORY="/usr/src/app/data/$repo"
             restic init
             cmd="restic backup /usr/src/app/data/$temp_file"
             file_compressed="$repo"
+            du -bcs "/usr/src/app/data/$repo" | grep total | awk '{print $1}' > "/usr/src/app/data/${repo}.size"
             ;;
         zbackup)
             suffix="zbackup"
-            repo="tmp/$base_name"
+            repo="tmp/${base_name}-${round}"
             mkdir -p "/usr/src/app/data/$repo"
             zbackup init --non-encrypted "/usr/src/app/data/$repo"
-
-            # Aplica o strace apenas no zbackup e redireciona a saída
             cmd="tar -cf - /usr/src/app/data/$temp_file 2>/dev/null | strace -c -e trace=read,write,open zbackup --non-encrypted backup /usr/src/app/data/$repo/backups/backup-$round 2> $result_path/$result_file"
             file_compressed="$repo"
             ;;
@@ -89,14 +106,10 @@ for tech in "${TECH_ARRAY[@]}"; do
         strace -c -e trace=read,write,open $cmd > /dev/null 2> $result_path/$result_file
     elif [[ "$tech" == "gzip" || "$tech" == "bzip2" ]]; then
         eval "strace -c -e trace=read,write,open $cmd" 2> $result_path/$result_file
-    elif [[ "$tech" == "borg" ]]; then
+    elif [[ "$tech" == "borg" || "$tech" == "restic" ]]; then  # Atualizado
         strace -c -e trace=read,write,open $cmd > /dev/null 2> $result_path/$result_file
-        du -bcs "/usr/src/app/data/$repo" > "/usr/src/app/data/$file_compressed"
-    elif [[ "$tech" == "restic" ]]; then
-        strace -c -e trace=read,write,open $cmd > /dev/null 2> $result_path/$result_file
-        du -bcs "/usr/src/app/data/$repo" | grep total | awk '{print $1}' > "/usr/src/app/data/$file_compressed"
     elif [[ "$tech" == "zbackup" ]]; then
-        eval "$cmd" 
+        eval "$cmd"
     fi
 
     echo "finding monitoramento.sh..."
@@ -104,36 +117,24 @@ for tech in "${TECH_ARRAY[@]}"; do
     kill -TERM $monitor_pid
     echo "killed monitoramento.sh..."
 
-    if [ "$temp_file" != "$origin_file" ]; then
-        if [ -e "/usr/src/app/data/$temp_file" ]; then
-            if [ -d "/usr/src/app/data/$temp_file" ]; then
-                rm -rf "/usr/src/app/data/$temp_file"
-            else
-                rm -f "/usr/src/app/data/$temp_file"
-            fi
-        fi
+    if [[ "$tech" == "gzip" || "$tech" == "bzip2" ]] && [ "$temp_file" != "$origin_file" ]; then
+        rm -f "/usr/src/app/data/$temp_file"
     fi
 
     temp_file=$file_compressed
 done
 
-echo "origin_file: /usr/src/app/data/$origin_file"
-echo "compressed_file: /usr/src/app/data/$file_compressed"
-
-# Detecta o tamanho do arquivo ou pasta de origem
 if [ -d "/usr/src/app/data/$origin_file" ]; then
     origin_size=$(du -bcs "/usr/src/app/data/$origin_file" | awk '/total/ {print $1}')
 else
     origin_size=$(stat -c %s "/usr/src/app/data/$origin_file")
 fi
 
-# Detecta o tamanho do arquivo ou pasta comprimido
-if [ -d "/usr/src/app/data/$file_compressed" ]; then
-    compressed_size=$(du -bcs "/usr/src/app/data/$file_compressed" | awk '/total/ {print $1}')
+if [[ -f "/usr/src/app/data/${temp_file}.size" ]]; then
+    compressed_size=$(cat "/usr/src/app/data/${temp_file}.size")
 else
     compressed_size=$(stat -c %s "/usr/src/app/data/$file_compressed")
 fi
-
 
 rate=$(echo "scale=4; 1 - ($compressed_size / $origin_size)" | bc)
 rate_percent=$(echo "scale=2; $rate * 100" | bc)
@@ -142,12 +143,6 @@ echo "" >> $result_path/$result_file
 echo "original reduzido taxa" >> $result_path/$result_file
 echo "$origin_size $compressed_size $rate_percent" >> $result_path/$result_file
 
-if [ -e "/usr/src/app/data/$file_compressed" ]; then
-    if [ -d "/usr/src/app/data/$file_compressed" ]; then
-        rm -rf "/usr/src/app/data/$file_compressed"
-    else
-        rm -f "/usr/src/app/data/$file_compressed"
-    fi
-fi
+find "/usr/src/app/data/tmp/" -name "*${round}*" -exec rm -rf {} \; 2>/dev/null
 
 echo "finished job"
